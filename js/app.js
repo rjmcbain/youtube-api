bcdce67e21628fbf2a6aa1908fab328fa5a6b941
rjmcbain <rmcbain01@gmail.com>
2018-07-20 14:01:59 -0600

    setting up js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 // Options
 const CLIENT_ID = '286229363565-3ssmvhlku6cljh12knc7sghsi5oguirl.apps.googleusercontent.com';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
-
-// Authorization scopes required by the API. If using multiple scopes,
-// separated them with spaces.
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
 const authorizeButton = document.getElementById('authorize-button');
 const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
+
+// Load auth2 library
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+
 
 
 
