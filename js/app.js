2cc7ac1b7948d8d4c447b9d58b4fbeccdb5c00a2
rjmcbain <rmcbain01@gmail.com>
2018-07-20 13:59:17 -0600

    setting up js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
+// Options
+const CLIENT_ID = '286229363565-3ssmvhlku6cljh12knc7sghsi5oguirl.apps.googleusercontent.com';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+
+// Authorization scopes required by the API. If using multiple scopes,
+// separated them with spaces.
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 AIzaSyDqZk8KhMvyhO_v-LxsBK0tTyHFRbohgsQ
 
-286229363565-3ssmvhlku6cljh12knc7sghsi5oguirl.apps.googleusercontent.com
+
 
 93BOo4m9oBTVZW7DGoMBSB9v
